123fa015a5e640db24881dcf6ed796aaeb4209db
jochoa <160277214+jochoa31@users.noreply.github.com>
2024-02-21 11:15:14 -0700

    Update cis129_lab02_shortStory.py with cat story
    Deleted Hello and added comments, and cat story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-('Hello, World!')
+#Author Jose Ochoa  
+#My first Python lab in my CIS 129 course
+#Prints a simple short story about a cat
+
+('The cat jumped on the wall')
+('The cat jumped off the wall')
+('The cat said "Meow" and licked it's paw,')
+('Then the cat jumped up again.')  
+
